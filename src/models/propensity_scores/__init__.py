a4a1ee7d033f9e16ba4ed3dde67df9be7e5cfbd0
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-08-07 11:13:32 +0200

    Propensity scores WIP

--- src/models/propensity_scores/__init__.py
+++ src/models/propensity_scores/__init__.py
@@ -6,7 +6,7 @@ from dowhy import CausalModel
 from dowhy.causal_estimators.propensity_score_weighting_estimator import (
     PropensityScoreWeightingEstimator,
 )
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import BaseEstimator, MultiOutputMixin, RegressorMixin
 from sklearn.linear_model import LinearRegression, LogisticRegression
 
 from src.cache import cache
@@ -25,45 +25,35 @@ class DummyModel(BaseEstimator, RegressorMixin):
         return X
 
 
-class DowhyWrapper(BaseEstimator, RegressorMixin):
+class DowhyWrapper(MultiOutputMixin, RegressorMixin, BaseEstimator):
     def __init__(
-        self,
-        df: pd.DataFrame,
-        y: str,
-        w: str,
-        x: list[str],
-        lagged_feature_names: list[
-            str
-        ] = None,  # this is needed because darts re-initializes the model 🙄😩🤬
-        **fit_args
+        self, df: pd.DataFrame, y: list[str], w: str, x: list[str], **fit_args
     ) -> None:
+        """
+        x: should include w, because it will be in the same dataframe
+        """
         self.df = df
         self.y = y
         self.w = w
         self.x = x
         self.fit_args = fit_args
-        self.lagged_feature_names = lagged_feature_names
 
     def fit(self, X, y):
-        assert isinstance(self.lagged_feature_names, list)
-        w_col = self.w + "_futcov_lag0"
-        x_col = [c for c in self.lagged_feature_names if c != w_col]
-        X = pd.DataFrame(X, columns=self.lagged_feature_names)
-        y = pd.DataFrame(y, columns=[self.y])
+        X = pd.DataFrame(X, columns=self.x)
+        y = pd.DataFrame(y, columns=self.y)
         df = y.join(X)
         self.df = df
-        model = CausalModel(
+        self.causal_model = CausalModel(
             data=df,
-            treatment=w_col,
+            treatment=self.w,
             outcome=self.y,
-            common_causes=x_col,
+            common_causes=[c for c in self.x if c != self.w],
             graph=None,
         )
-        estimand = model.identify_effect()
+        self.estimand = self.causal_model.identify_effect()
         self.estimator = PropensityScoreWeightingEstimator(
-            estimand, propensity_score_model=LogisticRegression()
-        )
-        self.estimator.fit(df, **self.fit_args)
+            self.estimand, propensity_score_model=LogisticRegression()
+        ).fit(df, **self.fit_args)
         return self
 
     def predict(self, X):
@@ -74,11 +64,12 @@ class DowhyWrapper(BaseEstimator, RegressorMixin):
 
 
 # @cache
-def propensity(lags: int, steps: int = 1, model=None):
+def propensity(w: str, lags: int, steps: int = 1):
     data = naive_all_regions()
     # y = [df[["media_online_protest"]].copy() for df in data.y]
     x = get_ts_list_with_statics(data.x)  # list of ts
     y = get_ts_list_with_statics(data.y)  # list of ts
+
     model_args = dict(
         lags=None if lags == 0 else lags,
         lags_future_covariates=(lags, 1),
@@ -90,7 +81,12 @@ def propensity(lags: int, steps: int = 1, model=None):
         .fit(**fit_args)
         .lagged_feature_names
     )
-    model.lagged_feature_names = feature_names
-    ts_model = RegressionModel(model=model, **model_args)
+    propensity_model = DowhyWrapper(
+        df=pd.DataFrame(),
+        y=y[0].columns,
+        w=w + "_futcov_lag0",
+        x=feature_names,
+    )
+    ts_model = RegressionModel(model=propensity_model, **model_args)
     ts_model.fit(**fit_args)
     return ts_model
