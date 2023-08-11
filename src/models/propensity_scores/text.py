ff388c211c28553b414c5dc0f60d927a00395ff7
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-08-11 23:22:16 +0200

    Bert training setup

--- src/models/propensity_scores/text.py
+++ src/models/propensity_scores/text.py
@@ -1,8 +1,12 @@
+import evaluate
 import numpy as np
 import pandas as pd
+import torch
+from datasets import Dataset
 from matplotlib import pyplot as plt
 from munch import Munch
 from nltk.corpus import stopwords
+from peft import LoraConfig, TaskType, get_peft_model
 from scipy.sparse import csr_matrix, hstack, vstack
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -14,9 +18,16 @@ from sklearn.model_selection import (
 )
 from sklearn.naive_bayes import ComplementNB
 from sklearn.pipeline import Pipeline
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
 
 from src.cache import cache
 from src.features.time_series import get_lagged_df
+from src.paths import models as model_path
 
 
 def get_data(cutoff: int | None = None):
@@ -44,12 +55,6 @@ def f1_ts():
     d = get_data()
     cvs = cross_val_score(d.clf, d.X_ts, d.y, cv=d.tscv, scoring="f1")
     print(f"Cross-validated F1 score: {cvs.mean():.3f} +/- {cvs.std():.3f}")
-
-
-# def shift_rows_downwards(matrix, n, fill_value=np.nan):
-#     empty_row = csr_matrix((n, matrix.shape[1]))
-#     shifted_matrix = vstack([empty_row, matrix[:-n, :]])
-#     return shifted_matrix
 
 
 @cache
@@ -115,8 +120,92 @@ def f1_augmented(cutoff: int | None = None):
     print(f"Cross-validated F1 score: {cvs.mean():.3f} +/- {cvs.std():.3f}")
 
 
-def train_transformer():
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
+
+
+@cache
+def load_dataset(model_name, max_length):
     d = get_data()
+    dataset = (
+        Dataset.from_dict(
+            dict(
+                text=d._X_text["text_lag0"],
+                labels=d.y,
+            )
+        )
+        .shuffle(seed=54325)
+        .select(range(2000))
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    dataset = dataset.map(
+        lambda sample: tokenizer(
+            sample["text"], padding="max_length", truncation=True, max_length=max_length
+        ),
+    )
+    ds = dataset.train_test_split(test_size=0.2)
+    train = ds["train"].with_format("torch")
+    test = ds["test"].with_format("torch")
+    return train, test
+
+
+f1_metric = evaluate.load("f1")
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return f1_metric.compute(predictions=predictions, references=labels)
+
+
+models = [
+    ("prajjwal1/bert-tiny", 512),
+    ("bert-base-german-cased", 512),
+    ("LennartKeller/longformer-gottbert-base-8192-aw512", 8192),
+]
+
+
+def train_bert(model_name, max_length):
+    train_dataset, test_dataset = load_dataset(model_name, max_length=max_length)
+    r = 1
+    lora_config = LoraConfig(
+        task_type=TaskType.SEQ_CLS, r=r, lora_alpha=r, lora_dropout=0.1
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+    model = get_peft_model(model, lora_config)
+    model.print_trainable_parameters()
+    path = model_path / "bert"
+    path.mkdir(exist_ok=True, parents=True)
+    trainer = Trainer(
+        model=model,
+        args=TrainingArguments(
+            output_dir=path,
+            learning_rate=2e-5,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=16,
+            num_train_epochs=3,
+            weight_decay=0.01,
+            warmup_ratio=0.1,
+            evaluation_strategy="steps",
+            save_steps=50,
+            eval_steps=50,
+            save_total_limit=5,
+            use_mps_device=get_device() == "mps",
+        ),
+        compute_metrics=compute_metrics,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+    )
+    trainer.train()
+    # trainer.save_model(path / "final")
+    model.save_pretrained(path / "final")
+    final_metrics = trainer.evaluate()
+    trainer.save_metrics("final", final_metrics)
 
 
 if __name__ == "__main__":
@@ -124,3 +213,4 @@ if __name__ == "__main__":
         import nltk
 
         nltk.download("stopwords")
+    train_bert(*models[0])
