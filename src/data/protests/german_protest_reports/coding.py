7957ced0194ccd06ab49a5f5c13312b3134ad768
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2023-08-23 23:50:18 +0200

    GPRep GPT

---
+++ src/data/protests/german_protest_reports/coding.py
@@ -0,0 +1,167 @@
+import json
+
+import holidays
+import pandas as pd
+from dateparser import parse
+from geopy.geocoders import Nominatim
+from joblib import Parallel, delayed
+from tqdm.auto import tqdm
+
+from src.cache import cache
+from src.data import german_region_names
+from src.data.protests.german_protest_reports.gpt import ask_gpt
+from src.paths import external_data, interim_data
+
+geolocator = Nominatim(user_agent="protest-impact")
+
+
+def read_data():
+    with open(external_data / "mediacloud/protest_news_predicted.jsonl") as f:
+        items = [json.loads(line) for line in tqdm(f)]
+    return items
+
+
+# path = interim_data / "german_protest_reports"
+# labelpath = path / "labels"
+# labelpath.mkdir(exist_ok=True, parents=True)
+
+schema = """
+{
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "IS_CLIMATE_PROTEST_EVENT": {
+                "type": "boolean"
+            },
+            "PAST_OR_FUTURE": {
+                "type": "string",
+                "enum": ["PAST", "FUTURE"]
+            },
+            "COUNTRY": {
+                "type": "string",
+                "enum": ["DE", "AT", "CH", "OTHER"]
+            },
+            "CITY": {
+                "type": "string"
+            },
+            "PROTEST_DATE_YEAR": {
+                "type": "integer"
+            },
+            "PROTEST_DATE_MONTH": {
+                "type": "string",
+                "enum": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+            },
+            "PROTEST_DATE_DAY": {
+                "type": "integer"
+            },
+            "PROTEST_GROUP": {
+                "type": "string",
+                "enum": ["FFF", "ALG", "XR", "EG", "GP", "OTHER_CLIMATE_GROUP"]
+            },
+            "N_PARTICIPANTS": {
+                "type": "integer"
+            },
+            "DESCRIPTION": {
+                "type": "string"
+            }
+        },
+        "required": ["IS_CLIMATE_PROTEST_EVENT"]
+    }
+}
+"""
+
+
+system_prompt = f"""
+Classify the following text as a climate protest event or not, and - if applicable - fill out the details.
+Use German-language city names.
+For the DESCRIPTION field, write a one-sentence summary of the event in English language, including the goal of the protest, the protest form, and - if applicable - confrontations with the police; as well as any other relevant information.
+The schema allows you to create multiple event objects in case that the text describes multiple events.
+Use the following schema:
+
+```json{schema}```
+"""
+
+
+@cache
+def get_region(city):
+    coordinates = geolocator.geocode(city + ", Germany")
+    if coordinates is None:
+        print(f"Could not find coordinates for {city}")
+        return None
+    regions = [r for r in german_region_names if r in coordinates.address]
+    return regions[0] if len(regions) > 0 else None
+
+
+holiday_names = [i[0] for i in holidays.Germany(years=range(2018, 2023)).items()]
+
+
+@cache
+def get_coding(row):
+    # get weekdays and holidays from the weeks before and after the protest
+    date_helper_text = ""
+    row.date = parse(row.date)
+    dates = pd.date_range(
+        row.date - pd.Timedelta(days=7), row.date + pd.Timedelta(days=7)
+    )
+    for date in dates:
+        # weekday
+        date_helper_text += f"{date.strftime('%A')}, "
+        # date
+        date_helper_text += f"{date.day}. {date.month_name()} {date.year}"
+        # holiday
+        if date in holidays.Germany(years=range(2018, 2023)):
+            date_helper_text += f", {holidays.Germany(years=range(2018, 2023))[date]}"
+        date_helper_text += "\n"
+    content = date_helper_text + "\n\n"
+    content += f"{row.title}\n\n{row.date.date()}\n\n{row.text}"
+    content += "\n\n```json\n"
+    return ask_gpt(system_prompt, content)
+
+
+@cache
+def parse_response(response):
+    try:
+        parsed_responses = []
+        responses_ = json.loads(response)
+        if isinstance(responses_, str):
+            return []
+        for parsed_response in responses_:
+            if not isinstance(parsed_response, dict):
+                continue
+            if parsed_response["IS_CLIMATE_PROTEST_EVENT"]:
+                parsed_response["CITY"] = parsed_response["CITY"].split(",")[0]
+                parsed_response["PROTEST_DATE"] = parse(
+                    f"{parsed_response['PROTEST_DATE_DAY']} {parsed_response['PROTEST_DATE_MONTH']} {parsed_response['PROTEST_DATE_YEAR']}"
+                )
+                parsed_response["REGION"] = get_region(parsed_response["CITY"])
+                parsed_response = {k.lower(): v for k, v in parsed_response.items()}
+                parsed_responses.append(parsed_response)
+        return parsed_responses
+    except json.decoder.JSONDecodeError as e:
+        print(e)
+        print(response)
+        print()
+        return []
+
+
+def coding(limit: int = None):
+    items = read_data()
+    climate_items = [i for i in items if "Klima" in i["title"] or "Klima" in i["text"]]
+    df = pd.DataFrame(climate_items)
+    responses = Parallel(n_jobs=1)(
+        delayed(get_coding)(row) for i, row in tqdm(list(df.iterrows())[:limit])
+    )
+    print("Done")
+    costs, responses = zip(*responses)
+    print(f"Total cost: {sum(costs)}")
+    parsed_responses = Parallel(n_jobs=4)(
+        delayed(parse_response)(response) for response in tqdm(responses)
+    )
+    items = [item for sublist in parsed_responses for item in sublist]
+    df = pd.DataFrame(items)
+    print(df.columns)
+
+
+coding()
--- src/data/protests/german_protest_reports/gpt.py
+++ src/data/protests/german_protest_reports/gpt.py
@@ -2,11 +2,13 @@ import os
 
 import openai
 from dotenv import load_dotenv
+from ratelimiter import RateLimiter
 
 from src.cache import cache
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+rate_limiter = RateLimiter(max_calls=2, period=1)
 
 
 @cache
@@ -14,8 +16,8 @@ def create_completion(**kwargs):
     return openai.ChatCompletion.create(**kwargs)
 
 
-def ask_gpt(prompt, user_msg, model="gpt-3.5-turbo"):
-    completion = create_completion(
+def _ask_gpt(prompt, user_msg, model="gpt-3.5-turbo"):
+    completion = rate_limiter(create_completion)(
         model=model,
         messages=[
             {
@@ -30,3 +32,12 @@ def ask_gpt(prompt, user_msg, model="gpt-3.5-turbo"):
     out_tokens = completion.usage.completion_tokens
     cost = in_tokens / 1_000 * 0.0015 + out_tokens / 1_000 * 0.002
     return cost, completion.choices[0].message.content.strip()
+
+
+def ask_gpt(prompt, user_msg, model="gpt-3.5-turbo"):
+    if len(user_msg.split()) > 3000:
+        user_msg = " ".join(user_msg.split()[:1000])
+    try:
+        return _ask_gpt(prompt, user_msg, model)
+    except openai.error.InvalidRequestError:
+        return None
